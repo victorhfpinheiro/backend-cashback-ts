651213288bb3d45401e000576684cbf3d6865ce4
Victor H F Pinheiro <victorhfp.bcc@gmail.com>
2020-07-27 17:32:58 -0300

    Criação de arquivos app.ts, server.ts e inclusão de arquivos com variaveis de ambiente

---
+++ src/app.ts
@@ -0,0 +1,48 @@
+import express from 'express'
+import cors from 'cors'
+import * as dotenv from 'dotenv'
+import routes from './routes'
+import { errors } from 'celebrate'
+import mongoose from 'mongoose'
+
+class App {
+  public express: express.Application;
+
+  public constructor () {
+    this.express = express()
+    this.middlewares()
+    this.database()
+    this.routes()
+    this.envs()
+  }
+
+  private middlewares (): void {
+    this.express.use(express.json())
+    this.express.use(cors())
+  }
+
+  private database (): void {
+    mongoose.connect(
+      process.env.DATABASE_URL,
+      {
+        dbName: process.env.DATABASE_NAME,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+      }
+    ).catch(err => {
+      console.error(err)
+    })
+  }
+
+  private routes (): void {
+    this.express.use(process.env.PREFIX_URL || '/api', routes)
+    this.express.use(errors())
+  }
+
+  private envs (): void {
+    dotenv.config()
+  }
+}
+
+export default new App().express
