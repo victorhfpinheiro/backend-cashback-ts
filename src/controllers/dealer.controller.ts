f4053fd0fa816b93d20bdc919dee3ae62f3796bf
Victor H F Pinheiro <victorhfp.bcc@gmail.com>
2020-07-27 21:57:25 -0300

    Criação do controller responsavel pelo revendedor

--- src/controllers/dealer.controller.ts
+++ src/controllers/dealer.controller.ts
@@ -1,8 +1,31 @@
 import { Request, Response } from 'express'
+import Dealer from '@schemas/dealer.schema'
+import authUtils from '@utils/auth.utils'
 
 class DealerController {
   async create (req: Request, res: Response) {
-    return res.status(200).send()
+    const { name, email, documentNumber, password } = req.body
+
+    const dealerExists = await Dealer.findOne({ documentNumber })
+
+    if (dealerExists) {
+      return res.status(400).send({ success: false, message: 'Revendedor já cadastrado no sistema.' })
+    }
+
+    const dealer = await Dealer.create({ name, email, documentNumber, password })
+
+    if (!dealer) {
+      return res.status(500).json({ success: false, message: 'Ops! Algo deu errado, tente novamente mais tarde!' })
+    }
+
+    dealer.password = undefined
+    const token = await authUtils.generateToken({ documentNumber: dealer.documentNumber })
+
+    return res.status(201).json({
+      success: true,
+      dealer,
+      token
+    })
   }
 }
 
